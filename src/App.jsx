b74a34e294d0bfaf68bd2db2764832b5e28a29f7
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-08-27 14:44:42 -0400

    cleaned jules work and added idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Homepage from './pages/HomePage';
 import Refpage from './pages/Refpage';
-import Chatpage from './pages/Chatpage';
+import Chatpage from './pages/ChatPage';
 import PythonPage from './pages/PythonPage';
 import JavascriptPage from './pages/JavascriptPage';
 import ReactPage from './pages/ReactPage';
@@ -14,7 +15,8 @@ const App = () => {
       <Navbar />
       <Routes>
         {/* Add comments for expandability */}
-        <Route path="/" element={<Refpage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/reference" element={<Refpage />} />
         <Route path="/chat" element={<Chatpage />} />
         <Route path="/python" element={<PythonPage />} />
         <Route path="/javascript" element={<JavascriptPage />} />
