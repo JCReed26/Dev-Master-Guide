a6f160e2765a02da5989d60d588d3c1584b512ce
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-08-27 10:02:49 -0400

    setup: daisyui, basic pages, navbar

---
+++ attempt-two/src/pages/Chatpage.jsx
@@ -0,0 +1,4 @@
+//NotebookLM like chatpage - perplexity spaces with chosen context
+
+//Selects stack from pinned references at top of refpage 
+//Takes in urls for docs and a scraper to find the other relevant docs sections
