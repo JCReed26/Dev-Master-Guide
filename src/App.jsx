9a2fcc3a3de53f39b1b6855ab1846bf52133f937
James Reed <117855743+JCReed26@users.noreply.github.com>
2025-08-28 16:56:51 -0400

    fixed refpage filename and imports, created terraform page, directly imported refpage onto homepage instead of double code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar';
+
+// Main Pages
 import Homepage from './pages/HomePage';
-import Refpage from './pages/Refpage';
+import Refpage from './pages/RefPage';
 import Chatpage from './pages/ChatPage';
+
+// Reference Pages
 import PythonPage from './pages/PythonPage';
 import JavascriptPage from './pages/JavascriptPage';
 import ReactPage from './pages/ReactPage';
 import CplusplusPage from './pages/CplusplusPage';
+import TerraformPage from './pages/TerraformPage';
 
 const App = () => {
   return (
@@ -22,6 +29,7 @@ const App = () => {
         <Route path="/javascript" element={<JavascriptPage />} />
         <Route path="/react" element={<ReactPage />} />
         <Route path="/c++" element={<CplusplusPage />} />
+        <Route path="/terraform" element={<TerraformPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/TerraformPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TerraformPage = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold">Terraform</h1>
+        </div>
+    )
+}
+
+export default TerraformPage;
